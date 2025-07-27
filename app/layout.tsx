434726d8e046afc3e55151103ee773d0a725de2f
quanuiter <23521258@gm.uit.edu.vn>
2025-07-27 14:37:14 +0700

    doi title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: '666 Words',
+  description: 'I make this shit for fun',
+  generator: 'Wuan',
 }
 
 export default function RootLayout({
